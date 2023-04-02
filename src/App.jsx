b066538b5758888418a42c15848ec067142497a5
Md:Ij <mdij455173@gmail.com>
2023-04-02 21:54:54 +0600

    start-templates

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      <div className="my-5 text-center">
+        <button className="btn btn-info ms-5">Info</button>
+        <button className="btn btn-success ms-5">Success</button>
+        <button className="btn btn-warning ms-5">Warning</button>
+        <button className="btn btn-error ms-5">Error</button>
+      </div>
+    </>
+  );
+}
+export default App;
