9112d4863529941e195fef29dd5c37108fda242d
Md:Ij <mdij455173@gmail.com>
2023-04-03 16:49:47 +0600

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className='text-5xl font-bold text-center text-blue-500 mt-5'>Hello React Router Dom</div>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
