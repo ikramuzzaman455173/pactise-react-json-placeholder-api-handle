b376f21f5bf13028d98f7708fdcc4b2dfc7fa5df
Md:Ij <mdij455173@gmail.com>
2023-04-23 22:00:24 +0600

    edit contact page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='my-container'>
+    <div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our About Components 😃</div>
+    </div>
   )
 }
 
